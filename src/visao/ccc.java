129ad281045c8ece6ae011e10a94a8dabdcda244
nicole conradi <nicole.c31@aluno.ifsc.edu.br>
2024-09-03 11:04:30 -0300

    imagem tela

--- src/visao/ccc.java
+++ src/visao/ccc.java
@@ -1,5 +1,29 @@
 package visao;
 
-public class ccc {
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JPanel;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+public class ccc extends JFrame {
+	
+	public static void main(String[] args) {
+		ccc c = new ccc();
+		c.setVisible(true);;
+	}
+	public ccc() {
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+
+		ImagePanel panel = new ImagePanel("src/img/aaa.png");
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(new MigLayout("", "[][][][][][][][][][][][][][]", "[][][][][][][]"));
+		// TODO Auto-generated constructor stub
+	}
 
 }
