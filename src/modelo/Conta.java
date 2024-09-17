52aeb57cc51bcc5de41589f96561008c147fd823
Isaque José <isaque.jp2007@aluno.ifsc.edu.br>
2024-09-17 12:00:20 -0300

    Código (não foi na tela principal)

---
+++ src/modelo/Conta.java
@@ -0,0 +1,32 @@
+package modelo;
+
+public class Conta {
+	public String nome;
+	public String email;
+	public Long cpf;
+	public String senha;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getCpf() {
+		return cpf;
+	}
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
