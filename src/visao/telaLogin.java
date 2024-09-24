6f70b153f33684353e1e5c8efe729697b089dd3a
nicole conradi <nicole.c31@aluno.ifsc.edu.br>
2024-09-24 08:50:37 -0300

    Ajuste dos componentes no redimensionamento Tela

--- src/visao/telaLogin.java
+++ src/visao/telaLogin.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
 import javax.swing.JFrame;
@@ -21,7 +22,7 @@ import java.awt.event.ActionEvent;
 
 public class telaLogin extends JFrame {
 	private static final long serialVersionUID = 1L;
-	private ImagePanel contentPane;
+	private JPanel contentPane;
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JLabel backgroundLabel;
@@ -48,47 +49,59 @@ public class telaLogin extends JFrame {
 	public telaLogin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1526, 1313);
-		contentPane = new ImagePanel("src/img/background1.png");
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
+		contentPane = new JPanel();
+		contentPane.setLayout(new GridLayout(1, 2));
+		setContentPane(contentPane);
+				
+				
+		ImagePanel panelDireito = new ImagePanel("src/img/background1D.png");
+		ImagePanel panelEsquerdo= new ImagePanel("src/img/background1E.png");
+
+		
+		contentPane.add(panelEsquerdo);
+		contentPane.add(panelDireito);
+		
+		
+		panelDireito.setBorder(new EmptyBorder(5, 5, 5, 5));
+        
 
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		setSize(screenSize.width, screenSize.height);
-		contentPane.setLayout(new MigLayout("", "[grow][grow][grow][grow][grow][grow 50][grow 50,fill][grow]", "[bottom][][][][][][][][][][][][::100px][::100px][][][][40px][][70px][][][][][][][][][70px][][][][][][][][][][][][][][][][][][][70px][]"));
+		panelDireito.setLayout(new MigLayout("", "[grow][grow 50][grow 50,fill][grow]", "[bottom][][][][][][][][][][][][::100px][::100px][][][][40px][][70px][][][][][][][][][70px][][][][][][][][][][][][][][][][][][][70px][]"));
 
 		JLabel lblBemVindo = new JLabel("Bem vindo");
 		lblBemVindo.setForeground(new Color(255, 255, 255));
 		lblBemVindo.setFont(new Font("Tahoma", Font.BOLD, 50));
-		contentPane.add(lblBemVindo, "cell 5 13,alignx left");
+		panelDireito.add(lblBemVindo, "cell 1 13,alignx left");
 		
 		JLabel lblNovamente = new JLabel("novamente!");
 		lblNovamente.setForeground(new Color(255, 255, 255));
 		lblNovamente.setFont(new Font("Tahoma", Font.BOLD, 50));
-		contentPane.add(lblNovamente, "cell 5 14,alignx left");
+		panelDireito.add(lblNovamente, "cell 1 14,alignx left");
 		
 		JLabel lblCpf = new JLabel("CPF:");
 		lblCpf.setForeground(new Color(255, 255, 255));
 		lblCpf.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		contentPane.add(lblCpf, "cell 5 18,alignx left");
+		panelDireito.add(lblCpf, "cell 1 18,alignx left");
 		textField_2 = new JTextField();
 		textField_2.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		contentPane.add(textField_2, "cell 5 19 2 1,grow");
+		panelDireito.add(textField_2, "cell 1 19 2 1,grow");
 		textField_2.setColumns(10);
 		
 		JLabel lblSenha = new JLabel("Senha:");
 		lblSenha.setForeground(new Color(255, 255, 255));
 		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		contentPane.add(lblSenha, "cell 5 27,alignx left");
+		panelDireito.add(lblSenha, "cell 1 27,alignx left");
 		textField_1 = new JTextField();
 		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		textField_1.setColumns(10);
-		contentPane.add(textField_1, "cell 5 28 2 1,alignx right,grow");
+		panelDireito.add(textField_1, "cell 1 28 2 1,alignx right,grow");
 		
 		JCheckBox chckbxMostrarSenha = new JCheckBox("Mostrar senha");
 		chckbxMostrarSenha.setForeground(new Color(255, 255, 255));
 		chckbxMostrarSenha.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		chckbxMostrarSenha.setBackground(new Color(32, 60, 115));
-		contentPane.add(chckbxMostrarSenha, "cell 5 29,alignx left");
+		panelDireito.add(chckbxMostrarSenha, "cell 1 29,alignx left");
 		
 		JButton btnEsqueciSenha = new JButton("Esqueci a senha");
 		btnEsqueciSenha.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -97,7 +110,7 @@ public class telaLogin extends JFrame {
 		btnEsqueciSenha.setFocusPainted(false);
 		btnEsqueciSenha.setBorderPainted(false);
 		btnEsqueciSenha.setBackground(new Color(32, 60, 115));
-		contentPane.add(btnEsqueciSenha, "cell 6 29,alignx right");
+		panelDireito.add(btnEsqueciSenha, "cell 2 29,alignx right");
 		JButton btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -106,11 +119,11 @@ public class telaLogin extends JFrame {
 		btnLogin.setForeground(Color.WHITE);
 		btnLogin.setFont(new Font("Tahoma", Font.BOLD, 30));
 		btnLogin.setBackground(new Color(103, 203, 239));
-		contentPane.add(btnLogin, "cell 5 47 2 1,alignx right,grow");
+		panelDireito.add(btnLogin, "cell 1 47 2 1,alignx right,grow");
 		JLabel lblNovaConta = new JLabel("Ainda não tem uma conta?");
 		lblNovaConta.setForeground(new Color(255, 255, 255));
 		lblNovaConta.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		contentPane.add(lblNovaConta, "cell 5 48,alignx left");
+		panelDireito.add(lblNovaConta, "cell 1 48,alignx left");
 		JButton btnCadastrese = new JButton("Cadastre-se");
 		btnCadastrese.setHorizontalAlignment(SwingConstants.RIGHT);
 		btnCadastrese.addActionListener(new ActionListener() {
@@ -122,7 +135,7 @@ public class telaLogin extends JFrame {
 		btnCadastrese.setFocusPainted(false);
 		btnCadastrese.setBorderPainted(false);
 		btnCadastrese.setBackground(new Color(32, 60, 115));
-		contentPane.add(btnCadastrese, "cell 6 48,alignx right");
+		panelDireito.add(btnCadastrese, "cell 2 48,alignx right");
 
 		setLocationRelativeTo(null); // Centraliza a janela na tela
 	}
