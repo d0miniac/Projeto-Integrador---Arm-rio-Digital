9713a8ff728c98ce6beb4a1a07ea231b258a9aa3
nicole conradi <nicole.c31@aluno.ifsc.edu.br>
2024-10-08 09:56:54 -0300

    Mudança dos botões da tela de menu

--- src/visao/RoundButton.java
+++ src/visao/RoundButton.java
@@ -9,37 +9,38 @@ import java.awt.geom.RoundRectangle2D;
 import javax.swing.JButton;
 
 public class RoundButton extends JButton {
-public RoundButton(String label) {
-    super(label);
-    Dimension size = getPreferredSize();
-    size.width = size.height = Math.max(size.width,size.height);
-    setPreferredSize(size);
+	
+	public RoundButton(String label) {
+		super(label);
+		Dimension size = getPreferredSize();
+		size.width = size.height = Math.max(size.width, size.height);
+		setPreferredSize(size);
 
-    setContentAreaFilled(false);
-}
+		setContentAreaFilled(false);
+	}
 
-protected void paintComponent(Graphics g) {
-    if (getModel().isArmed()) {
-        g.setColor(Color.lightGray);
-    } else {
-        g.setColor(getBackground());
-    }
-    g.fillRoundRect(0, 0, 250, 50, 40, 40);
-    super.paintComponent(g);
-}
+	protected void paintComponent(Graphics g) {
+		if (getModel().isArmed()) {
+			g.setColor(Color.lightGray);
+		} else {
+			g.setColor(getBackground());
+		}
+		g.fillRoundRect(0, 0, 250, 50, 40, 40);
+		super.paintComponent(g);
+	}
 
-protected void paintBorder(Graphics g) {
-    g.setColor(getForeground());
-    g.drawRoundRect(0, 0, 250, 50, 10, 10); 
-}
+	protected void paintBorder(Graphics g) {
+		g.setColor(getForeground());
+		g.drawRoundRect(0, 0, 250, 50, 10, 10);
+	}
 
-Shape shape;
-public boolean contains(int x, int y) {
-    if (shape == null || 
-            !shape.getBounds().equals(getBounds())) {
-        shape = new RoundRectangle2D.Float(0, 0, 250, 50,10,10);
-    }
-    return shape.contains(x, y);
-}
+	Shape shape;
+
+	public boolean contains(int x, int y) {
+		if (shape == null || !shape.getBounds().equals(getBounds())) {
+			shape = new RoundRectangle2D.Float(0, 0, 250, 50, 10, 10);
+		}
+		return shape.contains(x, y);
+	}
 
 }
