b12dbde8a9cf3b0fb03da03dee8c680789f465d1
Isaque José <isaque.jp2007@aluno.ifsc.edu.br>
2024-10-22 15:47:43 -0300

    a

--- src/controle/ProdutoDAO.java
+++ src/controle/ProdutoDAO.java
@@ -7,20 +7,31 @@ import modelo.Produto;
 
 
 public class ProdutoDAO {
-	public void cadastrarProduto(Produto p){
+	public int cadastrarProduto(Produto p){
 		PreparedStatement stmt1 = null;
 		int res1=0;
 		Connection conn = ConexaoBD.getConexaoMySQL();
 		try {
-			stmt1 = conn.prepareStatement( "insert into armariodigital.produtos(Tamanho,Categoria,Preco,QT_Estoque,Cor,Marca,idFornecedor,) values (?, ?, ?,?,?,?,1);");
-			stmt1.setString(1, p.getTamanho());
-			stmt1.setString(2, p.getCategoria());
-			stmt1.setFloat(3, p.getPreco());
-			stmt1.setInt(4, p.getQuantidade());
-			stmt1.setString(5, p.getCor());
-			stmt1.setString(6, p.getMarca());
+			int forn = 1;
+			stmt1 = conn.prepareStatement( "insert into armariodigital.produtos(idProduto,Tamanho,Categoria,Preco,QT_Estoque,Cor,Marca,Fornecedor_idFornecedor,Imagem) values (?,?, ?, ?,?,?,?,?,?);");
+			stmt1.setLong(1, p.getId());
+			stmt1.setString(2, p.getTamanho());
+			stmt1.setString(3, p.getCategoria());
+			stmt1.setFloat(4, p.getPreco());
+			stmt1.setInt(5, p.getQuantidade());
+			stmt1.setString(6, p.getCor());
+			stmt1.setString(7, p.getMarca());
+			stmt1.setLong(8, p.getFornecedor());
+			stmt1.setString(9,p.getFoto());
+			res1 = stmt1.executeUpdate();
+			
+			stmt1.close();
+			conn.close();
+			System.out.println("Produto Cadastrado");
 		} catch (Exception e) {
+			e.printStackTrace();
 			// TODO: handle exception
 		}
+		return res1;
 	}
 }
