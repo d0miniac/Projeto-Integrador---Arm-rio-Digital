f187c686ad810f677b760299c10fe89083bdb972
leticia.gb@aluno.ifsc.edu.br <leticia.gb@aluno.ifsc.edu.br>
2024-11-27 11:52:59 -0300

    ajustes pesquisa e tela vendas

--- src/controle/FornecedorDAO.java
+++ src/controle/FornecedorDAO.java
@@ -9,81 +9,131 @@ import modelo.Fornecedor;
 
 public class FornecedorDAO {
 
-    public int cadastrarFornecedor(Fornecedor fornecedor) {
-        String sql = "INSERT INTO fornecedores (Email, Nome_Fornecedor, Nome_Ctt, Telefone) VALUES (?, ?, ?, ?)";
-        try (Connection conn = ConexaoBD.getConexaoMySQL();
-             PreparedStatement stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
+	public int cadastrarFornecedor(Fornecedor fornecedor) {
+		String sql = "INSERT INTO fornecedores (Email, Nome_Fornecedor, Nome_Ctt, Telefone) VALUES (?, ?, ?, ?)";
+		try (Connection conn = ConexaoBD.getConexaoMySQL();
+				PreparedStatement stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
 
-            stmt.setString(1, fornecedor.getEmail());
-            stmt.setString(2, fornecedor.getNomeFornecedor());
-            stmt.setString(3, fornecedor.getNomeCtt());
-            stmt.setString(4, fornecedor.getTelefone());
+			stmt.setString(1, fornecedor.getEmail());
+			stmt.setString(2, fornecedor.getNomeFornecedor());
+			stmt.setString(3, fornecedor.getNomeCtt());
+			stmt.setString(4, fornecedor.getTelefone());
 
-            int affectedRows = stmt.executeUpdate();
-            if (affectedRows > 0) {
-                try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
-                    if (generatedKeys.next()) {
-                        fornecedor.setIdFornecedor(generatedKeys.getLong(1));
-                    }
-                }
-            }
-            return affectedRows;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return 0;
-        }
-    }
+			int affectedRows = stmt.executeUpdate();
+			if (affectedRows > 0) {
+				try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+					if (generatedKeys.next()) {
+						fornecedor.setIdFornecedor(generatedKeys.getLong(1));
+					}
+				}
+			}
+			return affectedRows;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
 
-    public void excluirFornecedor(Long idFornecedor) throws SQLException {
-        ProdutoDAO produtoDAO = new ProdutoDAO();
+	public void excluirFornecedor(Long idFornecedor) throws SQLException {
+		ProdutoDAO produtoDAO = new ProdutoDAO();
 
-        produtoDAO.excluirProdutos(idFornecedor);
+		produtoDAO.excluirProdutos(idFornecedor);
 
-        String sql = "DELETE FROM fornecedores WHERE idFornecedor = ?";
-        try (Connection conn = ConexaoBD.getConexaoMySQL();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setLong(1, idFornecedor);
-            stmt.executeUpdate();
-        }
-    }
+		String sql = "DELETE FROM fornecedores WHERE idFornecedor = ?";
+		try (Connection conn = ConexaoBD.getConexaoMySQL(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setLong(1, idFornecedor);
+			stmt.executeUpdate();
+		}
+	}
 
-    public void alterarFornecedor(Fornecedor fornecedor) throws SQLException {
-        String sql = "UPDATE fornecedores SET Email = ?, Nome_Fornecedor = ?, Nome_Ctt = ?, Telefone = ? WHERE idFornecedor = ?";
-        try (Connection conn = ConexaoBD.getConexaoMySQL();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+	public void alterarFornecedor(Fornecedor fornecedor) throws SQLException {
+		String sql = "UPDATE fornecedores SET Email = ?, Nome_Fornecedor = ?, Nome_Ctt = ?, Telefone = ? WHERE idFornecedor = ?";
+		try (Connection conn = ConexaoBD.getConexaoMySQL(); PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-            stmt.setString(1, fornecedor.getEmail());
-            stmt.setString(2, fornecedor.getNomeFornecedor());
-            stmt.setString(3, fornecedor.getNomeCtt());
-            stmt.setString(4, fornecedor.getTelefone());
-            stmt.setLong(5, fornecedor.getIdFornecedor());
+			stmt.setString(1, fornecedor.getEmail());
+			stmt.setString(2, fornecedor.getNomeFornecedor());
+			stmt.setString(3, fornecedor.getNomeCtt());
+			stmt.setString(4, fornecedor.getTelefone());
+			stmt.setLong(5, fornecedor.getIdFornecedor());
 
-            System.out.println("Alterando fornecedor com ID: " + fornecedor.getIdFornecedor());
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+			System.out.println("Alterando fornecedor com ID: " + fornecedor.getIdFornecedor());
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public ArrayList<Fornecedor> selecionarFornecedores() throws SQLException {
-        ArrayList<Fornecedor> fornecedores = new ArrayList<>();
-        String sql = "SELECT * FROM Fornecedores";
-        try (Connection conn = ConexaoBD.getConexaoMySQL();
-             PreparedStatement stmt = conn.prepareStatement(sql);
-             ResultSet rs = stmt.executeQuery()) {
+	public ArrayList<Fornecedor> selecionarFornecedores() {
+		ArrayList<Fornecedor> fornecedores = new ArrayList<>();
+		String sql = "SELECT * FROM Fornecedores";
+		try (Connection conn = ConexaoBD.getConexaoMySQL();
+				PreparedStatement stmt = conn.prepareStatement(sql);
+				ResultSet rs = stmt.executeQuery()) {
 
-            while (rs.next()) {
-                Fornecedor fornecedor = new Fornecedor();
-                fornecedor.setIdFornecedor(rs.getLong("idFornecedor"));
-                fornecedor.setNomeCtt(rs.getString("Nome_Ctt"));
-                fornecedor.setNomeFornecedor(rs.getString("Nome_Fornecedor"));
-                fornecedor.setEmail(rs.getString("Email"));
-                fornecedor.setTelefone(rs.getString("Telefone"));
-                fornecedores.add(fornecedor);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return fornecedores;
-    }
+			while (rs.next()) {
+				Fornecedor fornecedor = new Fornecedor();
+				fornecedor.setIdFornecedor(rs.getLong("idFornecedor"));
+				fornecedor.setNomeCtt(rs.getString("Nome_Ctt"));
+				fornecedor.setNomeFornecedor(rs.getString("Nome_Fornecedor"));
+				fornecedor.setEmail(rs.getString("Email"));
+				fornecedor.setTelefone(rs.getString("Telefone"));
+				fornecedores.add(fornecedor);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return fornecedores;
+	}
+
+	public ArrayList<Fornecedor> pesquisarFornecedores(String filtro) {
+		ArrayList<Fornecedor> listaFornecedores = new ArrayList<>();
+		String sql = "SELECT * FROM fornecedores WHERE 1=1";
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		Connection conn = ConexaoBD.getConexaoMySQL();
+
+		try {
+
+			if (filtro != null && !filtro.isEmpty()) {
+				sql += " AND (Nome_Fornecedor LIKE ? OR Nome_Ctt LIKE ? OR Email LIKE ? OR Telefone LIKE ?OR idFornecedor LIKE ?)";
+			}
+
+			stmt = conn.prepareStatement(sql);
+
+			if (filtro != null && !filtro.isEmpty()) {
+				stmt.setString(1, "%" + filtro + "%");
+				stmt.setString(2, "%" + filtro + "%");
+				stmt.setString(3, "%" + filtro + "%");
+				stmt.setString(4, "%" + filtro + "%");
+				stmt.setString(5, "%" + filtro + "%");
+			}
+
+			rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				Fornecedor fornecedor = new Fornecedor();
+				fornecedor.setIdFornecedor(rs.getLong("idFornecedor"));
+				fornecedor.setNomeFornecedor(rs.getString("Nome_Fornecedor"));
+				fornecedor.setNomeCtt(rs.getString("Nome_Ctt"));
+				fornecedor.setEmail(rs.getString("Email"));
+				fornecedor.setTelefone(rs.getString("Telefone"));
+				listaFornecedores.add(fornecedor);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (stmt != null)
+					stmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return listaFornecedores;
+	}
 }
--- src/controle/ProdutoDAO.java
+++ src/controle/ProdutoDAO.java
@@ -12,16 +12,16 @@ import modelo.Marca;
 import modelo.Produto;
 import modelo.Tamanho;
 
-
 public class ProdutoDAO {
 
-	public int cadastrarProduto(Produto p){
+	public int cadastrarProduto(Produto p) {
 		PreparedStatement stmt1 = null;
-		int res1=0;
+		int res1 = 0;
 		Connection conn = ConexaoBD.getConexaoMySQL();
 		try {
-			stmt1 = conn.prepareStatement( "insert into armariodigital.produtos(Tamanho,Categoria,Preco,QT_Estoque,Cor,Marca,Fornecedor_idFornecedor,Imagem) values (?, ?, ?,?,?,?,?,?);");
-			
+			stmt1 = conn.prepareStatement(
+					"insert into armariodigital.produtos(Tamanho,Categoria,Preco,QT_Estoque,Cor,Marca,Fornecedor_idFornecedor,Imagem) values (?, ?, ?,?,?,?,?,?);");
+
 			stmt1.setString(1, p.getTamanho().getDescricao());
 			stmt1.setString(2, p.getCategoria().getDescricao());
 			stmt1.setFloat(3, p.getPreco());
@@ -29,9 +29,9 @@ public class ProdutoDAO {
 			stmt1.setString(5, p.getCor().getDescricao());
 			stmt1.setString(6, p.getMarca().getDescricao());
 			stmt1.setLong(7, p.getFornecedor());
-			stmt1.setString(8,p.getFoto());
+			stmt1.setString(8, p.getFoto());
 			res1 = stmt1.executeUpdate();
-			
+
 			stmt1.close();
 			conn.close();
 		} catch (Exception e) {
@@ -40,7 +40,7 @@ public class ProdutoDAO {
 		}
 		return res1;
 	}
-	
+
 	public ArrayList<Produto> selecionarProdutos() {
 		ArrayList<Produto> listaProdutos = new ArrayList<>();
 		String sql = "SELECT * FROM Produtos;";
@@ -48,33 +48,32 @@ public class ProdutoDAO {
 		ResultSet rs = null;
 		Connection conn = ConexaoBD.getConexaoMySQL();
 		try {
-			stmt1= conn.prepareStatement("SELECT * FROM armariodigital.Produtos;");
+			stmt1 = conn.prepareStatement("SELECT * FROM armariodigital.Produtos;");
 			rs = stmt1.executeQuery();
-		while(rs.next()) {
-			Produto p = new Produto();
-			p.setId(rs.getLong("idProduto"));
-			p.setCategoria(Categoria.getCategoriaPorDescricao(rs.getString("Categoria")));
-			p.setCor(Cor.getCorPorDescicao(rs.getString("Cor")));
-			p.setTamanho(Tamanho.getTamanhoPorDescricao(rs.getString("Tamanho")));
-			p.setPreco(rs.getFloat("Preco"));
-			p.setQuantidade(rs.getInt("QT_Estoque"));
-			p.setMarca(Marca.getMarcaPorDescricao(rs.getString("Marca")));
-			p.setFornecedor(rs.getLong("Fornecedor_idFornecedor"));
-			p.setFoto(rs.getString("Imagem"));
-			listaProdutos.add(p);
-			
-			
-		}
-			
+			while (rs.next()) {
+				Produto p = new Produto();
+				p.setId(rs.getLong("idProduto"));
+				p.setCategoria(Categoria.getCategoriaPorDescricao(rs.getString("Categoria")));
+				p.setCor(Cor.getCorPorDescicao(rs.getString("Cor")));
+				p.setTamanho(Tamanho.getTamanhoPorDescricao(rs.getString("Tamanho")));
+				p.setPreco(rs.getFloat("Preco"));
+				p.setQuantidade(rs.getInt("QT_Estoque"));
+				p.setMarca(Marca.getMarcaPorDescricao(rs.getString("Marca")));
+				p.setFornecedor(rs.getLong("Fornecedor_idFornecedor"));
+				p.setFoto(rs.getString("Imagem"));
+				listaProdutos.add(p);
+
+			}
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return listaProdutos;
-		
+
 	}
-	
+
 	public void excluirProdutos(Long id) throws SQLException {
 		PreparedStatement stmt1 = null;
 		ResultSet rs = null;
@@ -90,51 +89,43 @@ public class ProdutoDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
-	
 	public void alterarProdutos(Produto p) throws SQLException {
 		PreparedStatement stmt1 = null;
 		ResultSet rs = null;
 		Connection conn = ConexaoBD.getConexaoMySQL();
 		try {
-			stmt1 = conn.prepareStatement("update armariodigital.produtos set Tamanho =?, Categoria = ?, Preco = ?, QT_Estoque = ?, Cor=?, Marca = ?, Fornecedor_idFornecedor=?,Imagem=? where idProduto = ?;");
-			
+			stmt1 = conn.prepareStatement(
+					"update armariodigital.produtos set Tamanho =?, Categoria = ?, Preco = ?, QT_Estoque = ?, Cor=?, Marca = ?, Fornecedor_idFornecedor=?,Imagem=? where idProduto = ?;");
+
 			stmt1.setString(1, p.getTamanho().getDescricao());
-			
-			
+
 			stmt1.setString(2, p.getCategoria().getDescricao());
-			
-			
+
 			stmt1.setFloat(3, p.getPreco());
-			
-			
+
 			stmt1.setInt(4, p.getQuantidade());
-			
-			
+
 			stmt1.setString(5, p.getCor().getDescricao());
-			
-			
+
 			stmt1.setString(6, p.getMarca().getDescricao());
-			
-			
+
 			stmt1.setLong(7, p.getFornecedor());
-			
-			
-			stmt1.setString(8,p.getFoto());
-			
-			
+
+			stmt1.setString(8, p.getFoto());
+
 			stmt1.setLong(9, p.getId());
-			
+
 			stmt1.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
+
 	public ArrayList<Produto> pesquisarProdutos(String filtro) {
 		ArrayList<Produto> listaProdutos = new ArrayList<>();
 		String sql = "SELECT * FROM Produtos;";
@@ -142,34 +133,34 @@ public class ProdutoDAO {
 		ResultSet rs = null;
 		Connection conn = ConexaoBD.getConexaoMySQL();
 		try {
-			stmt1= conn.prepareStatement("SELECT * FROM armariodigital.Produtos WHERE Categoria like ? OR Cor like ? OR Tamanho like? OR Marca like ?;");
-			stmt1.setString(1,filtro);
-			stmt1.setString(2,filtro);
-			stmt1.setString(3,filtro);
-			stmt1.setString(4,filtro);
+			stmt1 = conn.prepareStatement(
+					"SELECT * FROM armariodigital.Produtos WHERE Categoria like ? OR Cor like ? OR Tamanho like? OR Marca like ?;");
+			stmt1.setString(1, filtro);
+			stmt1.setString(2, filtro);
+			stmt1.setString(3, filtro);
+			stmt1.setString(4, filtro);
 			rs = stmt1.executeQuery();
-		while(rs.next()) {
-			Produto p = new Produto();
-			p.setId(rs.getLong("idProduto"));
-			p.setCategoria(Categoria.getCategoriaPorDescricao(rs.getString("Categoria")));
-			p.setCor(Cor.getCorPorDescicao(rs.getString("Cor")));
-			p.setTamanho(Tamanho.getTamanhoPorDescricao(rs.getString("Tamanho")));
-			p.setPreco(rs.getFloat("Preco"));
-			p.setQuantidade(rs.getInt("QT_Estoque"));
-			p.setMarca(Marca.getMarcaPorDescricao(rs.getString("Marca")));
-			p.setFornecedor(rs.getLong("Fornecedor_idFornecedor"));
-			p.setFoto(rs.getString("Imagem"));
-			listaProdutos.add(p);
-			
-			
-		}
-			
+			while (rs.next()) {
+				Produto p = new Produto();
+				p.setId(rs.getLong("idProduto"));
+				p.setCategoria(Categoria.getCategoriaPorDescricao(rs.getString("Categoria")));
+				p.setCor(Cor.getCorPorDescicao(rs.getString("Cor")));
+				p.setTamanho(Tamanho.getTamanhoPorDescricao(rs.getString("Tamanho")));
+				p.setPreco(rs.getFloat("Preco"));
+				p.setQuantidade(rs.getInt("QT_Estoque"));
+				p.setMarca(Marca.getMarcaPorDescricao(rs.getString("Marca")));
+				p.setFornecedor(rs.getLong("Fornecedor_idFornecedor"));
+				p.setFoto(rs.getString("Imagem"));
+				listaProdutos.add(p);
+
+			}
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return listaProdutos;
-		
+
 	}
 }
