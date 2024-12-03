a0bc08c95d230cb880baa2dc3e69056163afaaba
henri <henrique.l19@aluno.ifsc.edu.br>
2024-12-03 11:11:56 -0300

    tela cadastro produto erro

--- src/visao/TelaProdutos.java
+++ src/visao/TelaProdutos.java
@@ -41,30 +41,30 @@ public class TelaProdutos extends JFrame {
 	private ArrayList<Produto> listaProdutos;
 	private ProdutoTableModel ptm;
 
-	 public static void main(String[] args) {
-		    EventQueue.invokeLater(() -> {
-		        try {
-		            Funcionario funcionario = new Funcionario(); 
-		            String mensagem = "Bem-vindo ao sistema!";
-		            TelaProdutos frame = new  TelaProdutos(funcionario, mensagem);
-		            frame.setVisible(true);
-		            frame.setSize(1215, 850);
-		            frame.setLocationRelativeTo(null);
-		        } catch (Exception e) {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+			try {
+				Funcionario funcionario = new Funcionario();
+				String mensagem = "Bem-vindo ao sistema!";
+				TelaProdutos frame = new TelaProdutos(funcionario, mensagem);
+				frame.setVisible(true);
+				frame.setSize(1215, 850);
+				frame.setLocationRelativeTo(null);
+			} catch (Exception e) {
 
-		            TelaErro telaErro = new TelaErro("Erro crítico: " + e.getMessage());
-		            telaErro.setVisible(true);
-		        }
-		    });
-		}
-	
-	public TelaProdutos(Funcionario func, String mensagem)  {
+				TelaErro telaErro = new TelaErro("Erro crítico: " + e.getMessage());
+				telaErro.setVisible(true);
+			}
+		});
+	}
+
+	public TelaProdutos(Funcionario func, String mensagem) {
 		listaProdutos = new ArrayList<>();
 		ProdutoDAO p = new ProdutoDAO();
 		listaProdutos = p.selecionarProdutos();
-		
+
 		setTitle("Produtos");
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		contentPane = new ImagePanel("src/img/bgProdutos.png");
 		contentPane.setBackground(new Color(243, 244, 240));
@@ -74,30 +74,27 @@ public class TelaProdutos extends JFrame {
 		setResizable(false);
 
 		contentPane.setLayout(new MigLayout("", "[grow,fill]", "[120px][grow]"));
-		
+
 		JPanel panelVazio = new JPanel();
 		panelVazio.setBackground(new Color(0, 0, 0));
 		panelVazio.setOpaque(false);
 		contentPane.add(panelVazio, "cell 0 0,grow");
 		panelVazio.setLayout(null);
-		
-		
-		
+
 		JPanel panelComponentes = new JPanel();
 		panelComponentes.setBackground(new Color(255, 0, 0));
 		panelComponentes.setOpaque(false);
 		contentPane.add(panelComponentes, "cell 0 1,grow");
 		panelComponentes.setLayout(new MigLayout("", "[][][][][grow][]", "[][][grow]"));
-		
-		
+
 		txtFiltro = new JTextField();
 		txtFiltro.setUI(new HintTextFieldUI("Pesquise por categoria, marca, cor ou tamanho"));
 		txtFiltro.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		txtFiltro.setBorder(new LineBorder(new Color(123, 150, 212), 2, true));
 		panelComponentes.add(txtFiltro, "flowx,cell 4 0,alignx left");
 		txtFiltro.setColumns(90);
-		txtFiltro.setPreferredSize(new Dimension(450,45));
-		
+		txtFiltro.setPreferredSize(new Dimension(450, 45));
+
 		JButton btnAdd = new JButton("Adicionar");
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -112,21 +109,22 @@ public class TelaProdutos extends JFrame {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
-				
+
 			}
+
 		});
+
 		btnAdd.setBackground(new Color(243, 244, 240));
 		btnAdd.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		btnAdd.setMinimumSize(new Dimension(150, 30));
 		btnAdd.setMaximumSize(new Dimension(150, 30));
 		btnAdd.setBorder(new LineBorder(new Color(123, 150, 212), 2, true));
 		panelComponentes.add(btnAdd, "flowx,cell 4 1,alignx left,aligny center");
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBorder(new LineBorder(new Color(123, 150, 212), 2, true));
 		panelComponentes.add(scrollPane, "cell 4 2,grow");
-		
+
 		JLabel lblSeta = new JLabel("");
 		lblSeta.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		lblSeta.setIcon(new ImageIcon(TelaCadastroProdutos.class.getResource("/img/de-volta.png")));
@@ -141,49 +139,41 @@ public class TelaProdutos extends JFrame {
 				TelaMenu tela = new TelaMenu(func, mensagem);
 				dispose();
 				tela.setVisible(true);
-				
+
 			}
 		});
 		tableProdutos = new JTable();
 		tableProdutos.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		tableProdutos.setGridColor(new Color(0,0,0));
+		tableProdutos.setGridColor(new Color(0, 0, 0));
 		tableProdutos.setBackground(new Color(123, 150, 212));
-		tableProdutos.setForeground(new Color(255,255,255));
-		
+		tableProdutos.setForeground(new Color(255, 255, 255));
+
 		ptm = new ProdutoTableModel(listaProdutos);
 		tableProdutos.setModel(ptm);
-		
+
 		theader();
-		
+
 		scrollPane.setViewportView(tableProdutos);
-		
+
 		JButton btnUpdate = new JButton("Alterar");
 		btnUpdate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				int i = tableProdutos.getSelectedRow();
-				
-				
-				
+
 				Long id = (Long) tableProdutos.getModel().getValueAt(i, 0);
 				Produto pdt = listaProdutos.get(i);// Produto();
 				/*
-				for (Produto produto : listaProdutos) {
-					if(produto.getId()==id) {
-						pdt.setId(produto.getId());
-						pdt.setCategoria(produto.getCategoria());
-						pdt.setCor(produto.getCor());
-						pdt.setFornecedor(produto.getFornecedor());
-						pdt.setFoto(produto.getFoto());
-						pdt.setMarca(produto.getMarca());
-						pdt.setPreco(produto.getPreco());
-						pdt.setQuantidade(produto.getQuantidade());
-						pdt.setTamanho(produto.getTamanho());
-					}
-				}*/
-				
+				 * for (Produto produto : listaProdutos) { if(produto.getId()==id) {
+				 * pdt.setId(produto.getId()); pdt.setCategoria(produto.getCategoria());
+				 * pdt.setCor(produto.getCor()); pdt.setFornecedor(produto.getFornecedor());
+				 * pdt.setFoto(produto.getFoto()); pdt.setMarca(produto.getMarca());
+				 * pdt.setPreco(produto.getPreco()); pdt.setQuantidade(produto.getQuantidade());
+				 * pdt.setTamanho(produto.getTamanho()); } }
+				 */
+
 				TelaEditarProdutos tela;
 				try {
-					tela = new TelaEditarProdutos(pdt,func);
+					tela = new TelaEditarProdutos(pdt, func);
 					dispose();
 					tela.setVisible(true);
 					tela.setSize(657, 425);
@@ -192,7 +182,7 @@ public class TelaProdutos extends JFrame {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
+
 			}
 		});
 		btnUpdate.setBackground(new Color(243, 244, 240));
@@ -201,34 +191,29 @@ public class TelaProdutos extends JFrame {
 		btnUpdate.setMaximumSize(new Dimension(150, 30));
 		btnUpdate.setBorder(new LineBorder(new Color(123, 150, 212), 2, true));
 		panelComponentes.add(btnUpdate, "cell 4 1");
-		
+
 		JButton btnDelete = new JButton("Deletar");
 		btnDelete.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				int r=JOptionPane.showConfirmDialog(null, "Deseja realmente excluir esse produto?", "Exclusão", JOptionPane.YES_NO_OPTION);
-				if(r==JOptionPane.YES_OPTION) {
+
+				int r = JOptionPane.showConfirmDialog(null, "Deseja realmente excluir esse produto?", "Exclusão",
+						JOptionPane.YES_NO_OPTION);
+				if (r == JOptionPane.YES_OPTION) {
 					int i = tableProdutos.getSelectedRow();
 					Long id = (Long) tableProdutos.getModel().getValueAt(i, 0);
-					
-					
-					
-					
+
 					try {
 						p.excluirProdutos(id);
 						listaProdutos = p.selecionarProdutos();
 						ptm = new ProdutoTableModel(listaProdutos);
 						tableProdutos.setModel(ptm);
-						
+
 					} catch (SQLException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
 				}
-				
-				
-				
-				
+
 			}
 		});
 		btnDelete.setBackground(new Color(243, 244, 240));
@@ -237,9 +222,7 @@ public class TelaProdutos extends JFrame {
 		btnDelete.setMaximumSize(new Dimension(150, 30));
 		btnDelete.setBorder(new LineBorder(new Color(123, 150, 212), 2, true));
 		panelComponentes.add(btnDelete, "cell 4 1");
-		
-		
-		
+
 		JButton btnPesquisa = new JButton("PESQUISAR");
 		btnPesquisa.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -267,16 +250,13 @@ public class TelaProdutos extends JFrame {
 		btnPesquisa.setMaximumSize(new Dimension(150, 30));
 		btnPesquisa.setBorder(new LineBorder(new Color(123, 150, 212), 2, true));
 	}
-	private void theader() {
-		JTableHeader thead= tableProdutos.getTableHeader();
-		thead.setForeground(new Color(123,150,212));
-		thead.setBackground(new Color(255,255,255));
-		thead.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		
-	}
-	
-	
-	
-	
-}
 
+	private void theader() {
+		JTableHeader thead = tableProdutos.getTableHeader();
+		thead.setForeground(new Color(123, 150, 212));
+		thead.setBackground(new Color(255, 255, 255));
+		thead.setFont(new Font("Tahoma", Font.PLAIN, 20));
+
+	}
+
+}
