637d9851ca6ba7d86d5be57cea42a46917a140de
Leticia Gehrke Bruda <leticia.gb@aluno.ifsc.edu.br>
2024-12-03 11:54:21 -0300

    ajustes gerais e título produto

--- src/modelo/ProdutoTableModel.java
+++ src/modelo/ProdutoTableModel.java
@@ -6,7 +6,7 @@ import javax.swing.table.AbstractTableModel;
 
 public class ProdutoTableModel extends AbstractTableModel {
 	private static final long serialVersionUID = 1L;
-	private static final String[] colunas = {"ID", "Categoria", "Marca", "Cor", "Tamanho", "Quantidade", "Pre\u00E7o"};
+	private static final String[] colunas = {"ID","Titulo", "Categoria", "Marca", "Cor", "Tamanho", "Quantidade", "Pre\u00E7o"};
 	private ArrayList<Produto> produtosLista;
 	public ProdutoTableModel(ArrayList<Produto> produtos) {
 		this.produtosLista = produtos;
@@ -23,23 +23,26 @@ public class ProdutoTableModel extends AbstractTableModel {
 		Produto produto = produtosLista.get(rowIndex);
 		if(columnIndex == 0) {
 			return produto.getId();
-		}
-		else if(columnIndex ==1) {
-			return produto.getCategoria();
+			
+		}else if(columnIndex ==1) {
+			return produto.getTitulo();
 		}
 		else if(columnIndex ==2) {
-			return produto.getMarca();
+			return produto.getCategoria();
 		}
 		else if(columnIndex ==3) {
-			return produto.getCor();
+			return produto.getMarca();
 		}
 		else if(columnIndex ==4) {
-			return produto.getTamanho();
+			return produto.getCor();
 		}
 		else if(columnIndex ==5) {
-			return produto.getQuantidade();
+			return produto.getTamanho();
 		}
 		else if(columnIndex ==6) {
+			return produto.getQuantidade();
+		}
+		else if(columnIndex ==7) {
 			return produto.getPreco();
 		}
 		
